456dade31cc06dfb31b95e6fa5fd2bac2a4798b0
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-15 14:07:51 +0000

    Deploying to pages from @ localizethedocs/pyo3-guide-l10n@45dad55b028a0fd58a670cac57ff7af8d8077dba 🚀

---
+++ ltd-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",      "Development"],
+    ["0",         "Release 0.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/pyo3-guide-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/pyo3-guide-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/pyo3-guide-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/pyo3-guide-l10n"],
+  ]
+};
